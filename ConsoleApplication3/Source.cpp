636578cf2b3db2b6d7f4c538e5d43ed85d138dc0
Сергей Самовол <s.samovol@outlook.com>
2015-10-21 22:17:01 +0300

    пред финал

--- ConsoleApplication3/Source.cpp
+++ ConsoleApplication3/Source.cpp
@@ -3,42 +3,12 @@
 #include <cstdlib>
 using namespace std;
 
-void main() 
-{
-	int main_array[100], temp_array[10], sub_array[10], m = 0;
-	srand(time(NULL));
-	for (int input_counter = 0;input_counter <= sizeof(main_array);input_counter++) 
-	{
-		main_array[input_counter] = rand();
-		//cout << main_array[input_counter]<<endl;
-	}
-	//cout << endl;
-	for (int sub_counter = 0; sub_counter <= 9; sub_counter++) //заполнение sub_array
-	{
-		for (int temp_counter = m; temp_counter <= 9+m; temp_counter++)  // заполнение temp_array
-		{
-			temp_array[temp_counter] = main_array[temp_counter];
-
-		}
-		m += 10;
-		sort(temp_array, sizeof(temp_array));
-		sub_array[sub_counter] = temp_array[0];
-	}
-	sort(sub_array, sizeof(temp_array));
-	for (int output_counter = 0;output_counter <= sizeof(sub_array);output_counter++) //вывод результата финальной сортировки
-	{
-		cout << sub_array[output_counter]<<"\n";
-	}
-	
-	
-}
-
 void sort(int* sort_array, int array_size) // сортировка выбором
 {
-	for (int sort_counter = 0; sort_counter < array_size; sort_counter++)
+	for (int sort_counter = 0; sort_counter <= array_size; sort_counter++)
 	{
 		int temp = sort_array[0]; // временная переменная для хранения значения перестановки
-		for (int sub_sort_counter = sort_counter + 1; sub_sort_counter < array_size; sub_sort_counter++)
+		for (int sub_sort_counter = sort_counter + 1; sub_sort_counter <= array_size; sub_sort_counter++)
 		{
 			if (sort_array[sort_counter] > sort_array[sub_sort_counter])
 			{
@@ -49,3 +19,40 @@ void sort(int* sort_array, int array_size) // сортировка выбором
 		}
 	}
 }
+
+void main() 
+{
+	int main_array[100], temp_array[10], sub_array[10], m = 0; //для первого задания
+	int var_for_ex2 = 0; //для второго задания
+	srand(time(NULL));
+	for (int input_counter = 0;input_counter <= 99;input_counter++) //заполнение первичного массива
+	{
+
+		main_array[input_counter] = rand();
+		var_for_ex2 += main_array[input_counter];
+		/*if (main_array[input_counter] == ex3_temp + 1) var_for_ex3 += 1;
+		ex3_temp = main_array[input_counter];*/
+		
+		//cout << main_array[input_counter]<<endl;
+	}
+	//cout << endl;
+	for (int sub_counter = 0; sub_counter <= 9; sub_counter++) //заполнение sub_array
+	{
+		for (int temp_counter = 0; temp_counter <= 9; temp_counter++)  // заполнение temp_array
+		{
+			temp_array[temp_counter] = main_array[temp_counter + m];
+		}
+		m += 10;
+		sort(temp_array, 9);
+		sub_array[sub_counter] = temp_array[0];
+	}
+	sort(sub_array, 9);
+	cout << "Ex.1: ";
+	for (int output_counter = 0;output_counter <= 9;output_counter++) //вывод результата финальной сортировки
+	{
+		cout << sub_array[output_counter]<<" ";
+	}
+	cout << endl<<"Ex.2: " << var_for_ex2 / 10<<endl;
+	
+}
+

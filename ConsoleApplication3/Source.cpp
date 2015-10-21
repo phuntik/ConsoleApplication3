09651487924fb04f633ca5a40f28497c6d004246
Сергей Самовол <s.samovol@outlook.com>
2015-10-21 03:45:40 +0300

    lal

--- ConsoleApplication3/Source.cpp
+++ ConsoleApplication3/Source.cpp
@@ -5,35 +5,44 @@ using namespace std;
 
 void main() 
 {
-	int main_mass[100], sub_mass[10], m = 0;
+	int main_array[100], temp_array[10], sub_array[10], m = 0;
 	srand(time(NULL));
-	for (int counter = 0;counter <= sizeof(main_mass);counter++) 
+	for (int input_counter = 0;input_counter <= sizeof(main_array);input_counter++) 
 	{
-		main_mass[i] = rand();
-		//cout << main_mass[i]<<endl;
+		main_array[input_counter] = rand();
+		//cout << main_array[input_counter]<<endl;
 	}
-	for (int sub_counter = 0; sub_counter <= 9; sub_counter++)  // заполнение sub_mass
+	//cout << endl;
+	for (int sub_counter = 0; sub_counter <= 9; sub_counter++) //заполнение sub_array
 	{
-		sub_mass[sub_counter] = main_mass[sub_counter + m];
-
-		
-
+		for (int temp_counter = 0; temp_counter <= 9; temp_counter++)  // заполнение temp_array
+		{
+			temp_array[temp_counter] = main_array[temp_counter + m];
+		}
+		sort(temp_array, sizeof(temp_array));
+		sub_array[sub_counter] = temp_array[0];
 	}
+	sort(sub_array, sizeof(temp_array));
+	for (int output_counter = 0;output_counter <= sizeof(sub_array);output_counter++) //вывод результата финальной сортировки
+	{
+		cout << sub_array[output_counter]<<"\n";
+	}
+	
 	
 }
 
-void sort(int* arrayPtr, int length_array) // сортировка выбором
+void sort(int* sort_array, int array_size) // сортировка выбором
 {
-	for (int repeat_counter = 0; repeat_counter < length_array; repeat_counter++)
+	for (int sort_counter = 0; sort_counter < array_size; sort_counter++)
 	{
-		int temp = arrayPtr[0]; // временная переменная для хранения значения перестановки
-		for (int element_counter = repeat_counter + 1; element_counter < length_array; element_counter++)
+		int temp = sort_array[0]; // временная переменная для хранения значения перестановки
+		for (int sub_sort_counter = sort_counter + 1; sub_sort_counter < array_size; sub_sort_counter++)
 		{
-			if (arrayPtr[repeat_counter] > arrayPtr[element_counter])
+			if (sort_array[sort_counter] > sort_array[sub_sort_counter])
 			{
-				temp = arrayPtr[repeat_counter];
-				arrayPtr[repeat_counter] = arrayPtr[element_counter];
-				arrayPtr[element_counter] = temp;
+				temp = sort_array[sort_counter];
+				sort_array[sort_counter] = sort_array[sub_sort_counter];
+				sort_array[sub_sort_counter] = temp;
 			}
 		}
 	}

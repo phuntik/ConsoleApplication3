677288b6caaaf84b94d95a375b8cc06972ff654e
Сергей Самовол <s.samovol@outlook.com>
2015-10-29 13:41:25 +0300

    final

--- ConsoleApplication3/Source.cpp
+++ ConsoleApplication3/Source.cpp
@@ -3,11 +3,11 @@
 #include <cstdlib>
 using namespace std;
 
-void sort(int* sort_array, int array_size) // сортировка выбором
+void sort(int* sort_array, int array_size) // ?????????? ???????
 {
 	for (int sort_counter = 0; sort_counter <= array_size; sort_counter++)
 	{
-		int temp = sort_array[0]; // временная переменная для хранения значения перестановки
+		int temp = sort_array[0]; // ????????? ?????????? ??? ???????? ???????? ????????????
 		for (int sub_sort_counter = sort_counter + 1; sub_sort_counter <= array_size; sub_sort_counter++)
 		{
 			if (sort_array[sort_counter] > sort_array[sub_sort_counter])
@@ -20,25 +20,30 @@ void sort(int* sort_array, int array_size) // сортировка выбором
 	}
 }
 
-void main() 
+void main()
 {
-	int main_array[100], temp_array[10], sub_array[10], m = 0; //для первого задания
-	int var_for_ex2 = 0; //для второго задания
+	int main_array[100], temp_array[10], sub_array[10], m = 0; //переменные для первого задания
+	int var_for_ex2 = 0; //переменные для второго задания
+	int var_for_ex3 = 0, ex3_temp = 0, ex3_counter = 0;
 	srand(time(NULL));
-	for (int input_counter = 0;input_counter <= 99;input_counter++) //заполнение первичного массива
+	for (int input_counter = 0;input_counter <= 99;input_counter++) //заполнение главного массива
 	{
 
 		main_array[input_counter] = rand();
+
 		var_for_ex2 += main_array[input_counter];
-		/*if (main_array[input_counter] == ex3_temp + 1) var_for_ex3 += 1;
-		ex3_temp = main_array[input_counter];*/
-		
+		/* задание 3 */
+		if (var_for_ex3 >= 10) ex3_counter++;
+		if (main_array[input_counter] == ex3_temp + 1) var_for_ex3 += 1;
+		else var_for_ex3 = 0;
+		ex3_temp = main_array[input_counter];
+
 		//cout << main_array[input_counter]<<endl;
 	}
 	//cout << endl;
-	for (int sub_counter = 0; sub_counter <= 9; sub_counter++) //заполнение sub_array
+	for (int sub_counter = 0; sub_counter <= 9; sub_counter++) //заполнение массива минимумов десяток
 	{
-		for (int temp_counter = 0; temp_counter <= 9; temp_counter++)  // заполнение temp_array
+		for (int temp_counter = 0; temp_counter <= 9; temp_counter++)  // заполнение временного массива для определения минимума десятки
 		{
 			temp_array[temp_counter] = main_array[temp_counter + m];
 		}
@@ -48,11 +53,11 @@ void main()
 	}
 	sort(sub_array, 9);
 	cout << "Ex.1: ";
-	for (int output_counter = 0;output_counter <= 9;output_counter++) //вывод результата финальной сортировки
+	for (int output_counter = 0;output_counter <= 9;output_counter++) //вывод минимумов десяток в порядке возрастания
 	{
-		cout << sub_array[output_counter]<<" ";
+		cout << sub_array[output_counter] << " ";
 	}
-	cout << endl<<"Ex.2: " << var_for_ex2 / 10<<endl;
-	
-}
+	cout << endl << "Ex.2: " << var_for_ex2 / 10 << endl;
+	cout << "Ex.3: " << ex3_counter << endl;
 
+}
